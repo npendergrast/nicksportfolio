ae60c31e62f6bf554a211f5b976ee465ed303cbc
npendergrast <55637835+npendergrast@users.noreply.github.com>
2019-12-20 18:31:48 +1300

    Add files via upload

---
+++ Javascript/index-javascript.js
@@ -0,0 +1,42 @@
+const image1 = new Image();
+image1.src = 'Images/me.jpg';
+const image2 = new Image();
+image2.src = 'Images/akl1.jpg';
+const image3 = new Image();
+image3.src = 'Images/akl2.jpg';
+
+
+
+var background_img = document.querySelector(".nick-background");
+var slideArray = ["url('Images/me.jpg')", "url('Images/akl1.jpg')", "url('Images/akl2.jpg')"];
+var index = 0;
+
+setTimeout(fadeOut, 5000);
+
+function fadeOut() {
+    background_img.classList.toggle("fade");
+    setTimeout(backgroundChange, 500);
+}
+
+function fadeIn() {
+    background_img.classList.toggle("fade");
+    setTimeout(fadeOut, 5000)
+}
+
+function backgroundChange() {
+    if (index == 0) {
+        background_img.style.backgroundImage = slideArray[1];
+        index = 1;
+        fadeIn();
+    } else if (index == 1) {
+        background_img.style.backgroundImage = slideArray[2];
+        index = 2;
+        fadeIn();
+    } else {
+        background_img.style.backgroundImage = slideArray[0];
+        index = 0;
+        fadeIn();
+    }
+}
+
+
